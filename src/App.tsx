6dfad80a2da6e324a9813353dcff09a83b8cc055
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-06-01 13:48:38 +0900

    ~#17.2 react, chakraui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
+import { Text } from '@chakra-ui/react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <Text color={"red"}>It works!!</Text>
+  </div>
 }
 
 export default App;
