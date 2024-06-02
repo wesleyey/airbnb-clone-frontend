9cb0fca19a7e458a390eccfd1243c2a9ea508cf8
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-06-02 16:37:04 +0900

    #18.2 sign up modal

---
+++ src/components/SocialLogin.tsx
@@ -0,0 +1,25 @@
+import { Box, HStack, Text, VStack, Button, Divider } from "@chakra-ui/react"
+import { FaComment, FaGithub } from "react-icons/fa"
+
+
+export default function SocialLogin() {
+    return (
+        <Box mb={4}>
+            <HStack my={8}>
+                <Divider />
+                <Text textTransform={"uppercase"} color={"gray.500"} fontSize={"x-small"} as={"b"}>
+                    Or
+                </Text>
+                <Divider />
+            </HStack>
+            <VStack>
+                <Button width="100%" leftIcon={<FaGithub />} colorScheme={"telegram"}>
+                    Continue with Github
+                </Button>
+                <Button width="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
+                    Continue with Kakao
+                </Button>
+            </VStack>
+        </Box>
+    )
+}
