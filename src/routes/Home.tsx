488bfc9b8d63903bd8d1a1d567a36864c0010259
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-06-03 23:12:31 +0900

    #19.0 manual fetching(annotaion added)

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -30,17 +30,25 @@ interface IRoom {
 }
 
 export default function Home() {
+    //useState 훅을 사용하여 두 개의 상태 변수를 정의
     const [isLoading, setIsLoading] = useState(true);
+    //isLoading: 초기값이 true로 설정되어 있으며, 데이터를 로딩 중인지 여부를 나타냄
     const [rooms, setRooms] = useState<IRoom[]>([]);
+    //rooms: IRoom 타입의 빈 배열로 초기화되며, 방의 목록을 저장
     const fetchRooms = async () => {
         const response = await fetch("http://127.0.0.1:8000/api/v1/rooms");
+        //fetch를 사용하여 지정된 URL("http://127.0.0.1:8000/api/v1/rooms")로부터 데이터를 가져옴
         const json = await response.json();
+        //응답을 JSON 형식으로 파싱
         setRooms(json);
         setIsLoading(false);
+        //가져온 데이터를 rooms 상태 변수에 설정하고, 로딩 상태를 false로 변경
     };
     useEffect(() => {
         fetchRooms();
+        //useEffect 훅을 사용하여 컴포넌트가 마운트될 때 fetchRooms 함수를 호출
     }, []);
+    //빈 배열 []을 두 번째 인자로 전달하여, 이 훅이 컴포넌트가 처음 렌더링될 때 한 번만 실행되도록 함
     return (
         <Grid
             mt={10}
